640d326b6328717faf1a024c611fcac60e97e2d3
OmarShawkey13 <81522507+OmarShawkey13@users.noreply.github.com>
2023-02-06 14:34:56 +0200

    Create SkyUpdate.js

---
+++ SkyUpdate.js
@@ -0,0 +1,14 @@
+{
+	"version": "2.0",
+	"download": "https://rcwaios.com/",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/OmarShawkey13/Update/main/os-update.html"
+		},
+		{
+			"languaje": "ar",
+			"changelog": "https://raw.githubusercontent.com/OmarShawkey13/Update/main/os-update-2.html"
+		}
+	]
+}
